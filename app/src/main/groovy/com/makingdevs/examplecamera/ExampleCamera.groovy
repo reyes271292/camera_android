f311365cfc7c385aecf9c6a62b4ad4a5b3250d49
Francisco Reyes <francisco@makingdevs.com>
2016-07-25 17:57:43 -0500

    Se agrega proyecto android con groovy

---
+++ app/src/main/groovy/com/makingdevs/examplecamera/ExampleCamera.groovy
@@ -0,0 +1,13 @@
+package com.makingdevs.examplecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExampleCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_camera);
+    }
+}
