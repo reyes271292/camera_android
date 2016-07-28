fbfda5838e5f78ff77500172c358b9cf9c614da5
Francisco Reyes <francisco@makingdevs.com>
2016-07-28 18:06:43 -0500

    Se agrega boton camara

--- app/src/main/groovy/com/makingdevs/examplecamera/ExampleCamera.groovy
+++ app/src/main/groovy/com/makingdevs/examplecamera/ExampleCamera.groovy
@@ -2,12 +2,23 @@ package com.makingdevs.examplecamera;
 
 import android.os.Bundle
 import android.support.v7.app.ActionBarActivity
+import android.widget.ImageButton
 
-public class ExampleCamera extends ActionBarActivity {
+class ExampleCamera extends ActionBarActivity {
+
+    ImageButton cameraImageButton
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example_camera)
+        bindingViews()
+    }
+
+    void bindingViews(){
+        cameraImageButton = (ImageButton) findViewById(R.id.imageButtonCamera)
+        cameraImageButton.onClickListener = {
+            println("Camera...")
+        }
     }
 }
